73e5490253b0f750813fefe6e8d58eab7c5d0125
abusayed22 <abusayed.in22@gmail.com>
2022-08-06 22:50:57 +0530

    ok

--- src/reducer-test/TestReducer.jsx
+++ src/reducer-test/TestReducer.jsx
@@ -1,32 +1,35 @@
 import { useReducer } from "react"
 
-const reducer = (state,action) => {
-    switch (action) {
-        case "inc":
-            return state + 1
+const reducer = (intialState, action) => {
+    switch (action.type) {
+        case "Increment":
+            return {count: intialState.count + action.payload}
             break;
 
-        case "dic":
-            return state -1
+        case "Diccrement":
+            return {count:intialState.count - action.payload}
             break;
     
         default:
-            return state ;
+            return intialState;
             break;
     }
 }
-const initialState = 6
+
+const intialState = {
+    count:10,
+}
 
 function TestReducer() {
+    const [counter, dispatch] = useReducer(reducer, intialState) 
 
-    const [counter, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <div>
+    <div style={{margin:'20px'}}>
         <center>
-            <center><b>counter:{counter}</b></center>
-            <button type="button" onClick={() => dispatch("inc")}>inc</button> <br />
-            <button type="button" onClick={() => dispatch("dic")}>dic</button>
+        <center><b>{counter.count}</b></center>
+        <button onClick={() => dispatch({type:"Increment",payload:3})} className="btn btn-sm btn-active btn-primary">Increment</button>
+        <button onClick={() => dispatch({type:"Diccrement",payload:3})} className="btn btn-sm btn-active btn-accent">Diccrement</button>
         </center>
     </div>
   )
